f5c1b3fa9ec21252e5cc0e15ec5ee0a7e4dc6b7c
kopanomothlaka <kopanomothlaka@gmail.com>
2024-10-09 12:51:14 +0200

    updated

--- resources/views/admin/events.blade.php
+++ resources/views/admin/events.blade.php
@@ -7,11 +7,11 @@
         <h1>Event Calendar</h1>
 
         <!-- Button to trigger the modal -->
-        <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#eventModal">
+        <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#eventModal" id="addEventBtn">
             Add Event
         </button>
 
-        <!-- Modal for adding events -->
+        <!-- Modal for adding/editing events -->
         <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -43,7 +43,8 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" id="addEventBtn">Add Event</button>
+                        <button type="button" class="btn btn-danger" id="deleteEventBtn" style="display: none;">Delete Event</button>
+                        <button type="button" class="btn btn-primary" id="saveEventBtn">Save Event</button>
                     </div>
                 </div>
             </div>
@@ -60,6 +61,8 @@
         <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js'></script>
 
         <script>
+            let currentEvent; // Variable to hold the currently selected event
+
             $(document).ready(function() {
                 // Initialize the calendar
                 var calendar = $('#calendar').fullCalendar({
@@ -75,11 +78,35 @@
                         var startTime = moment(event.start).format('HH:mm');
                         var endTime = moment(event.end).format('HH:mm');
                         element.find('.fc-title').append('<br/>' + startTime + ' - ' + endTime);
+                    },
+                    eventClick: function(event) {
+                        // Populate the modal with event data for editing
+                        currentEvent = event;
+                        $('#eventTitle').val(event.title);
+                        $('#eventStartDate').val(moment(event.start).format('YYYY-MM-DD'));
+                        $('#eventStartTime').val(moment(event.start).format('HH:mm'));
+                        $('#eventEndDate').val(moment(event.end).format('YYYY-MM-DD'));
+                        $('#eventEndTime').val(moment(event.end).format('HH:mm'));
+
+                        $('#eventModalLabel').text('Edit Event');
+                        $('#deleteEventBtn').show(); // Show delete button
+                        $('#saveEventBtn').text('Update Event'); // Change button text
+                        $('#eventModal').modal('show');
                     }
                 });
 
                 // Add event button click
                 $('#addEventBtn').click(function() {
+                    // Reset the form for a new event
+                    currentEvent = null;
+                    $('#eventForm')[0].reset(); // Reset the form
+                    $('#deleteEventBtn').hide(); // Hide delete button
+                    $('#eventModalLabel').text('Add Event');
+                    $('#saveEventBtn').text('Add Event');
+                });
+
+                // Save or update event button click
+                $('#saveEventBtn').click(function() {
                     var title = $('#eventTitle').val();
                     var startDate = $('#eventStartDate').val();
                     var startTime = $('#eventStartTime').val();
@@ -92,24 +119,45 @@
                         var start = moment(startDate + ' ' + startTime, 'YYYY-MM-DD HH:mm');
                         var end = moment(endDate + ' ' + endTime, 'YYYY-MM-DD HH:mm');
 
-                        var event = {
-                            title: title,
-                            start: start,
-                            end: end
-                        };
+                        if (currentEvent) {
+                            // Update existing event
+                            currentEvent.title = title;
+                            currentEvent.start = start;
+                            currentEvent.end = end;
 
-                        // Render the event on the calendar
-                        calendar.fullCalendar('renderEvent', event, true);
+                            calendar.fullCalendar('updateEvent', currentEvent);
+                        } else {
+                            // Create new event
+                            var event = {
+                                title: title,
+                                start: start,
+                                end: end
+                            };
 
-                        // Save the event to localStorage
-                        var events = JSON.parse(localStorage.getItem('events')) || [];
-                        events.push(event);
+                            // Render the event on the calendar
+                            calendar.fullCalendar('renderEvent', event, true);
+                        }
+
+                        // Save the events to localStorage
+                        var events = calendar.fullCalendar('clientEvents');
                         localStorage.setItem('events', JSON.stringify(events));
 
-                        $('#eventModal').modal('hide');
-                        $('#eventForm')[0].reset(); // Reset the form
+                        $('#eventModal').modal('hide'); // Hide the modal
                     } else {
                         alert('Please fill in all fields correctly (HH:MM format for time).');
+                    }
+                });
+
+                // Delete event button click
+                $('#deleteEventBtn').click(function() {
+                    if (currentEvent) {
+                        calendar.fullCalendar('removeEvent', currentEvent._id); // Remove event from calendar
+
+                        // Save the events to localStorage
+                        var events = calendar.fullCalendar('clientEvents');
+                        localStorage.setItem('events', JSON.stringify(events));
+
+                        $('#eventModal').modal('hide'); // Hide the modal
                     }
                 });
             });
